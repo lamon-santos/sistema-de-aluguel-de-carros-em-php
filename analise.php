cef371eee1ed2dfe584deaa2459eb242cbcbc479
Lamon Santos <developerdartflutter@gmail.com>
2025-01-04 14:54:48 -0300

    Esse código foi últilizado em um trabalho de faculdade, fiz a alteração para se adaptar ao sistema.

--- analise.php
+++ analise.php
@@ -21,49 +21,25 @@
                 $pesquisa = $_GET["pesquisa"];
                 
                 switch($pesquisa) {
-                    case "Analista de Sistemas":
-                        echo "<h2>Analista de Sistemas</h2><br>
-                                <p>O analista de sistemas ou atualmente mais conhecido como sistematizador de informações, é aquele que tem como finalidade realizar estudos de processos computacionais para encontrar o melhor e mais racional caminho para que a informação virtual possa ser processada.</p>
-                                <p>Este profissional estuda os diversos sistemas existentes entre hardwares e softwares e o usuário final, incluindo seus comportamentos e aplicações.</p>
-                                <p>Formação recomendada: Curso Superior em Ciência da Computação, Informática ou Engenharia da Computação ou qualquer outra área de base matemática.</p>
-                                <p><b>Salário: Entre R$ 4.800 a R$ 9.100 reais.</b></p>
-                                <br><br>";
+                    case "hatch":
+                        echo "<h2>O que é tipo de veículo hatch?</h2>
+                                <p>Carros do tipo hatch​ são divididos em dois volumes principais: a caixa do motor e o bloco dos passageiros, que fica junto com o porta-malas.</p>
+                                <p>Na prática, isso significa apenas que o automóvel é mais compacto.</p>
+                                <p>Uma das primeiras vantagens do hatch é o seu tamanho compacto, que pode facilitar muito na hora de conseguir uma vaga. Além disso, o carro hatch oferece melhor percepção das dimensões do próprio veículo.</p>";
                         break;
-                    case "Administrador de Banco de Dados":
-                        echo "<h2>Administrador de Banco de Dados</h2><br>
-
-                                <p>O Administrador de banco de dados (DBA - DataBase Administrator) é responsável por manter e gerenciar bancos de dados, ou sistema de banco de dados.</p>
-                                <p>Este profissional gerencia, atualiza, monitora o centro das informações de um sistema. Formação recomendada: Curso Superior em Ciência da Computação, Informática ou Engenharia da Computação ou qualquer outra área de base matemática. Atividades: Criação e testes de backup para garantir a recuperabilidade dos dados no caso de falha de hardware ou outros problemas severos. Verificar e zelar pela integridade do banco de dados.</p>
-                                <p>Ter um controle de acesso aos dados como quem pode acessar e o que pode acessar e talvez quando possa acessar. Garantir o acesso ao banco de dados no maior tempo possível. Garantir o máximo de desempenho para o banco de dados. Auxiliar a equipe de desenvolvimento e a equipe de testes a maximizar o uso e desempenho do banco de dados.</p>
-                                <p><b>Salário: Entre R$ 3.000 a R$ 9.000 reais.</b></p>
-                                <br><br>";
+                    case "sedan":
+                        echo "<h2>Carro para toda família</h2>
+                                <p>Com seu espaço interno e um generoso porta-malas, sedans são a opção ideal para famílias que usam o carro para suas tarefas diárias ou para viagens de longa duração. Um sedan comporta confortavelmente as crianças e até mesmo animais de estimação, se necessário.</p>
+                                <p>As principal vantagem em adquirir um carro sedan é o conforto, oferecido por um espaço interno maior, e a capacidade do porta-malas — ideal para quem possui filhos e precisa de mais espaço para as bagagens.</p>";
                         break;
-                    case "Analista de Suporte":
-                        echo "		<h2>Analista de Suporte</h2><br>
-                                <p>O analista de suporte é um profissional de TI especialista em tecnologias, constantemente atualizado com novidades mercadológicas de Hardware e Software.</p>
-                                <p>Cuida da manutenção da estrutura física de computadores, da estrutura de Rede de área local de computadores e de sistemas operacionais.</p>
-                                <p>Atividades: Instalar e manter os diversos Sistemas Operacionais; Instalar e manter a comunicação digital (correio eletrônico, WEB, FTP, VPN, etc.): Instalar e manter sistemas de gestão (ERP); Instalar e manter sistemas de banco de dados (SGBD); Suporte aos usuários da empresa</p>
-                                <p><b>Salário: Entre R$ 2.000 a R$ 7.000 reais.</b></p>
-                                <br><br>";
+                    case "suv":
+                        echo "<h2>SUV</h2>
+                                <p>Em sua definição básica, SUV é a sigla para 'Sport Utility Vehicle', ou 'Veículo Utilitário Esportivo', ou seja, um carro destinado a carga/trabalho, como as picapes, mas com uma tocada mais esportiva. Normalmente é equipado com tração nas quatro rodas e caracteristicamente de porte avantajado.</p>
+                                <p>Qual a diferença de um carro SUV?</p>
+                                <p>Os SUVs são veículos que têm mais adaptabilidade a diferentes terrenos, são mais altos e robustos e, também, confortáveis e arrojados. <p>O motor, no geral, é mais potente e alguns modelos têm, ainda, a opção de tração 4×4.</p>";
                         break;
-                    case "Programador Web":
-                        echo "<h2>Programador Web</h2><br>
-                                <p>Um profissional de programação web é responsável pelo desenvolvimento de sites, portais, fóruns e aplicações voltadas para o ambiente da internet.</p>
-                                <p>Formação recomendada: 2o grau técnico ou faculdade de Informática/Processamento de Dados/Engenharia da Computação.</p>
-                                <p>Atividades: Programação do código de sites, portais, sistemas para a internet; Cuidar da segurança de uma aplicação na internet; Aplicar formas de melhorar a performance do site; Criar sistemas de backend de softwares; Portar aplicações desktop para a web.</p>
-                                <p><b>Salário: Entre R$ 2.500 a R$ 5.000.</b></p>
-                                <br><br>";
-                        break;
-                    case "Programador Desktop":
-                        echo "		<h2>Programador Desktop</h2><br>
-                                <p>Um programador pode ser alguém que desenvolve ou faz manutenção de software.</p>
-                                <p>Formação recomendada: Para ser programador de computador é necessário diploma de curso técnico, que pode ser cursado paralelamente ou após a conclusão do ensino médio.</p> 
-                                <p>Por ser um mercado competitivo, entretanto, cursos extras são importantes para diferenciar o currículo na hora da seleção.</p>
-                                <p>Atividades: listar ou ler as especificações de programas, detalhadas por um analista de sistemas, e que mostram passo a passo as tarefas que o computador precisa executar; analisar o problema e a melhor forma para solucioná-lo; preparar diagramas para mostrar a seqüência de procedimentos a ser adotada pela máquina; codificar essas instruções para uma linguagem de computador; depois de prontas e implantadas as instruções, o programador deve testar todo o sistema, através de simulação, para verificar falhas e possíveis adequações.</p> 
-                                <p><b>Salário: Entre R$ 1.800 a R$ 9.000 reais.</b></p>
-                                <br><br>";
                     default:
-                        echo "Profissão não encontrada.";
+                        echo "Carro não encontrado!";
                         break;
                 }
 
