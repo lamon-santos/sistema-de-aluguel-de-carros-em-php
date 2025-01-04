9f15c9f44921c5bb8b3a8beb6800172d6a614703
Lamon Santos <developerdartflutter@gmail.com>
2025-01-04 16:47:33 -0300

    fatoração do código

--- aluguel.php
+++ aluguel.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Programação em PHP</title>
     <link rel="stylesheet" href="css/estilo.css">
     <style type="text/css">
-
         .formulario {
             background-color: transparent;
             color: black;
@@ -32,11 +32,11 @@
             outline: none;
         }
 
-        #grid-container{
-            background-color:rgb(136, 196, 255);
+        #grid-container {
+            background-color: rgb(136, 196, 255);
         }
 
-        .grid{
+        .grid {
             display: grid;
             grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
             grid-gap: 5px;
@@ -44,7 +44,7 @@
             margin: 0 auto;
         }
 
-        .grid div{
+        .grid div {
             background-color: #fff;
             padding: 20px;
             border-radius: 8px;
@@ -54,12 +54,12 @@
             padding-bottom: 25px;
         }
 
-        img{
+        img {
             width: 95%;
             height: 80%;
         }
 
-        a{
+        a {
             color: red;
             padding: 10px;
             border-radius: 8px;
@@ -69,87 +69,54 @@
             font: bold;
         }
 
-        fieldset{
-           border: solid;
-           padding: 5%;
-           border-radius: 15px;
+        fieldset {
+            border: solid;
+            padding: 5%;
+            border-radius: 15px;
         }
-
     </style>
 </head>
+
 <body>
     <header>
         <h3>Programação em PHP</h3>
         <h4>Aluguel de Carros</h4>
     </header>
     <br>
-        <section>
-            <h1>Aluguel de Carros - Curso de PHP</h1><br>
-           <div class="formulario">
-                <form action="analise.php" method="get">
-                
-                <label for="pesquisa">Pesquisar:</label>&nbsp;
-                <input type="text" name="pesquisa" id="pesquisa" placeholder="Digite Aqui Sua Pesquisa"> <!-- input é o campo de entrada de dados, type é o tipo de dado que será inserido, name é o nome do campo, id é o identificador do campo, placeholder é o texto que aparece no campo-->
+    <br>
+    <section>
+        <?php
+        $nome = isset($_GET["nome"]) ? $_GET["nome"] : '';
+        $data = isset($_GET["data"]) ? $_GET["data"] : '';
+        $email = isset($_GET["email"]) ? $_GET["email"] : '';
+        $telefone = isset($_GET["telefone"]) ? $_GET["telefone"] : '';
+        $carro = isset($_GET["carro"]) ? $_GET["carro"] : '';
+        $tempo = isset($_GET["tempo"]) ? $_GET["tempo"] : '';
+        $modelocarro;
+        $valordoaluguel;
 
-                <input type="submit" name="enviar" value="Pesquisar"> <!-- input é o campo de entrada de dados, type é o tipo de dado que será inserido, value é o texto que aparece no botão-->
-                </form>
-           </div>
-        </section>
+        switch ($carro) {
+            case "hatch":
+                $modelocarro = "Hatch";
+                $valordoaluguel = $tempo * 100;
+                break;
+            case "sedan":
+                $modelocarro = "Sedan";
+                $tempo * 150;
+                $valordoaluguel = $tempo * 150;
+                break;
+            case "suv":
+                $modelocarro = "SUV";
+                $tempo * 200;
+                $valordoaluguel = $tempo * 200;
+                break;
+            default:
+                $modelocarro = "Não encontrado";
+                $valordoaluguel = 0;
+                break;
+        }
 
-        <br>
-
-        <section class="container" id="grid-container">
-           <div class="grid">
-                <div>
-                    <h2>Hatch</h2>
-                    <img src="img/hatch.png" alt="Foto de um carro Hatch">
-                    <a href="hatch.html">CLIQUE AQUI PARA ALUGAR</a>
-                </div>
-                <div>
-                    <h2>Sedan</h2>
-                    <img src="img/sedan.png" alt="Foto de um carro Sedan">
-                    <a href="sedan.html">CLIQUE AQUI PARA ALUGAR</a>
-                </div>
-                <div>
-                    <h2>SUV</h2>
-                    <img src="img/suv.png" alt="Foto de um carro SUV">
-                    <a href="suv.html">CLIQUE AQUI PARA ALUGAR</a>
-                </div>
-           </div> 
-        </section>
-                <section>
-                    <?php
-                        $nome = isset($_GET["nome"]) ? $_GET["nome"] : '';
-                        $data = isset($_GET["data"]) ? $_GET["data"] : '';
-                        $email = isset($_GET["email"]) ? $_GET["email"] : '';
-                        $telefone = isset($_GET["telefone"]) ? $_GET["telefone"] : '';
-                        $carro = isset($_GET["carro"]) ? $_GET["carro"] : '';
-                        $tempo = isset($_GET["tempo"]) ? $_GET["tempo"] : '';
-                        $modelocarro;
-                        $valordoaluguel;
-
-                        switch($carro) {
-                            case "hatch":
-                                $modelocarro = "Hatch";
-                                    $valordoaluguel = $tempo * 100;
-                                break;
-                            case "sedan":
-                                $modelocarro = "Sedan";
-                                    $tempo * 150;
-                                        $valordoaluguel = $tempo * 150;
-                                break;
-                            case "suv":
-                                $modelocarro = "SUV";
-                                    $tempo * 200;
-                                        $valordoaluguel = $tempo * 200;
-                                break;
-                            default:
-                                $modelocarro = "Não encontrado";
-                                    $valordoaluguel = 0;
-                                break;
-                        }
-
-                        echo "<fieldset>
+        echo "<fieldset>
                                 <legend>
                                     <h2>Aluguel de Carros</h2>
                                 </legend>
@@ -162,20 +129,22 @@
                                 <p><strong>Tempo:</strong> $tempo horas</p>
                                 <p><strong>Valor do Aluguel:</strong> R$ $valordoaluguel</p>
                             </fieldset>";
-                    ?>
-                <br>
-                <a href="index.php"> <<< VOLTAR A PÁGINA ANTERIOR</a>
-            </section>
-        <footer> <!-- serve para colocar o rodapé do site-->
-            <h3>ALUGUEL DE CARROS</h3>
-            <h4>FAÇA SEU ORÇAMENTO JÁ</h4>
-            <br>
-            <p class="texto-rodape">Contatos</p>
-            <p class="texto-rodape">E-mail: contato@exemplo.com</p>
-            <p class="texto-rodape">Telefone: (11) 9 8888-8888</p>
-            <br>
-            <p class="texto-rodape">Desenvolvido por: <a href="https://www.linkedin.com/in/exemplo/">Nome do Desenvolvedor</a></p>
-            <p class="texto-rodape">CNPJ: 00.000.000/0000-00</p>
-        </footer>
-    </body>
+        ?>
+        <br>
+        <a href="index.php">
+            <<< VOLTAR A PÁGINA ANTERIOR</a>
+    </section>
+    <footer> <!-- serve para colocar o rodapé do site-->
+        <h3>ALUGUEL DE CARROS</h3>
+        <h4>FAÇA SEU ORÇAMENTO JÁ</h4>
+        <br>
+        <p class="texto-rodape">Contatos</p>
+        <p class="texto-rodape">E-mail: contato@exemplo.com</p>
+        <p class="texto-rodape">Telefone: (11) 9 8888-8888</p>
+        <br>
+        <p class="texto-rodape">Desenvolvido por: <a href="https://www.linkedin.com/in/exemplo/">Nome do Desenvolvedor</a></p>
+        <p class="texto-rodape">CNPJ: 00.000.000/0000-00</p>
+    </footer>
+</body>
+
 </html>
